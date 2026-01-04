5d5ea7b5baebfcd9e0909fba6387c9e6899d7f1f
dylcruz <dylan@dylancruz.me>
2026-01-03 18:03:07 -0800

    finish chap 18

--- project 2/pizza_project/pizzas/views.py
+++ project 2/pizza_project/pizzas/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 def index(request):
+    """Home page for Pizza App"""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    """Show all pizzas."""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Show speific pizza and toppings"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("-date_added")
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
